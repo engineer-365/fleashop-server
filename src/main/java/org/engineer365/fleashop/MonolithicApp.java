cdcd1ec68baf10f044497dcddea36d53a9c634cf
Yiting Qiang <qiangyt@wxcount.com>
2021-01-05 15:15:03 +0800

    rollback: remove spring-cloud-feign

--- src/main/java/org/engineer365/fleashop/MonolithicApp.java
+++ src/main/java/org/engineer365/fleashop/MonolithicApp.java
@@ -28,7 +28,6 @@ import org.engineer365.platform.user.app.entity.UserEO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -36,7 +35,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackageClasses = UserEO.class)
 @EnableJpaRepositories(basePackageClasses = UserDAO.class)
 @EnableJpaAuditing
-@EnableFeignClients
 public class MonolithicApp {
 
 	public static void main(String[] args) {
